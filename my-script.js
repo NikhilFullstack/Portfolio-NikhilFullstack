9eb32253a99b3e644014e55d13c17f8b2b579cc5
Nikhil Gupta <Gup7nik@gmail.com>
2023-05-17 18:06:46 +0530

    fds

---
+++ my-script.js
@@ -0,0 +1,20 @@
+const form=document.querySelector('#form');
+
+form.addEventListener('submit',async (e)=>{
+    e.preventDefault();
+    const formData=new FormData(form);
+    const data=Object.fromEntries(formData.entries());
+    console.log(data); 
+        console.log("kahani shuru ho gyi")
+    const savedUserResponse = await fetch(
+        `https://portfolio-t127.onrender.com/api/v1/createUser`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ ...data }),
+        })
+      console.log("FORM RESPONSE......", savedUserResponse);
+      alert("Form Submitted Successfully")
+})
